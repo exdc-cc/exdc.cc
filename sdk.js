33d6b2314f5ebb55cab314c64df4166c146658f8
Daniel <fiend.of.sound@gmail.com>
2024-08-27 00:42:07 +0200

    feat: update sdk

---
+++ sdk.js
@@ -0,0 +1,435 @@
+import {EXDC_UTILS} from "./util.js"
+function EXDC_SDK(params = {}) {
+
+  let categories = [
+    // 'All', 'Electronics', 'Clothing', 'Books', 'Home & Garden', 'Sports'
+  ];
+  // Get USDT balance (replace with actual USDT contract address on BSC Testnet)
+  
+  const utils = new EXDC_UTILS()
+  let products = [];
+  let orderKey;
+  let provider, signer, address;
+  const n = params.n || (new URL(window.location.href)).searchParams.get("n") || 137
+  let erc20ContractAddress = utils.exchangeTokenAddress(); // Example USDT contract on BSC Testnet
+  let decimals = 18;
+  let storedJSON = {};
+  const address0 = '0x0000000000000000000000000000000000000000';
+  
+  const getExchangeContractData = async (q) => {
+    const exchangeContract = new ethers.Contract(q, utils.exchangeContractAbi, provider);
+    const data = await exchangeContract.userData();
+    erc20ContractAddress = await exchangeContract.wp()
+    decimals = await exchangeContract.decimals()
+    const json = await processExchangeContractData(data)
+    storedJSON = json;
+    if(json.encrypted) {
+      return json;
+    } else {
+      initContent(json)
+    }
+    return json;
+  }
+  
+  const inputPassword = async (password, json = storedJSON) => {
+    const decr = await utils.decrypt(password, json)
+    initContent(decr)
+    return decr;
+  }
+
+  const processExchangeContractData = async (data) => {
+    const abiCoder = new ethers.utils.AbiCoder();
+    try {
+      const decode = await abiCoder.decode(["string"], data)
+      return JSON.parse(decode);
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  async function rateSeller(rating) {
+    const order = await getCurrentOrder()
+    await (await order.rateSeller(rating)).wait(1)
+  }
+  
+  
+  const generateOrderBody = async (nload) => {
+    const abiCoder = new ethers.utils.AbiCoder();
+    const aeskey = await generateAesKey();
+    const keypair = await generateNewKeyPair();
+    nload.publicKey = keypair.publicKey;
+    const payload = await encryptPayload(aeskey.key, JSON.stringify(nload))
+    localStorage.setItem("easkp", JSON.stringify(keypair))
+    const encKey = await encryptAesKey(aeskey.kp, orderKey)
+    const strData = JSON.stringify({ encKey, payload })
+    const data = abiCoder.encode(["string"], [strData])
+    const a = document.createElement("a");
+    a.href = URL.createObjectURL(new Blob([JSON.stringify(keypair, null, 2)], {
+      type: "text/plain"
+    }));
+    a.setAttribute("download", "orderKey.json");
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    return data;
+  }
+  
+  const getCurrentOrder = async (q) => {
+    const signer = await provider.getSigner();
+    const shop = new ethers.Contract(q, utils.exchangeContractAbi, signer);
+    const prevOrder = await shop.buyerOrders(address)
+    // console.info('currentOrder', prevOrder)
+    if (prevOrder !== address0) {
+      const contract = new ethers.Contract(prevOrder, buyContractAbi, signer);
+      contract.contractAddress = prevOrder;
+      return contract;
+    }
+    return undefined;
+  }
+  
+  const buyItemFromShop = async (
+    q,
+    value,
+    deliveryAddress,
+    cart
+  ) => {
+    const signer = await provider.getSigner();
+    const shop = new ethers.Contract(q, exchangeContractAbi, signer);
+    const coin = await shop.wp();
+    const nload = {cart, deliveryAddress}
+    // console.info('nload', nload)
+    let contract;
+    try {
+      contract = await getCurrentOrder()
+      if (contract) {
+        const state = ethers.utils.formatUnits(await contract.state(), 10) * 10
+        if(state > 2) {
+          return true;
+        }
+        const {contractAddress} = contract;
+        const balance = ethers.utils.formatUnits(await contract.balanceOfContract(), decimals)
+        const price = ethers.utils.formatUnits(await contract.price(), decimals)
+        // console.info("balance", balance, price, contract, contractAddress)
+        if (balance < price) {
+          await sendCoin(coin, contractAddress, price - balance);
+        }
+      } else {
+        // console.info('creating a new contract', value)
+        const contractTx = (await (
+          await (
+            await shop.createBuyItemsContract(value, 1, q)
+          ).wait(1)
+        ));
+        contract = await getCurrentOrder()
+        const trx = await sendCoin(coin, contract.contractAddress, value);
+      }
+      const data = await generateOrderBody(nload)
+      const purchase = await (await contract.confirmPurchase(data)).wait(1);
+      return purchase;
+    } catch (err) {
+      console.error(err)
+      alert(err?.data?.message || err.message || err)
+    }
+    
+    const purchase = await contract.confirmPurchase(data);
+    return trx;
+  };
+  
+  async function sendCoin(
+    coin,
+    to,
+    valueNum,
+  ) {
+    const erc20 = new ethers.Contract(coin, utils.erc20abi, await provider.getSigner());
+    const amount = ethers.utils.parseUnits(
+      valueNum.toString(),
+      decimals,
+    );
+    const tx = await (await erc20.transfer(to, amount)).wait(1);
+    return tx.hash;
+  }
+  
+  
+  const byteToHex = [];
+  
+  for (let n = 0; n <= 0xff; ++n)
+  {
+      const hexOctet = n.toString(16).padStart(2, "0");
+      byteToHex.push(hexOctet);
+  }
+  const receiveOrder = async () => {
+    try {
+      const order = await getCurrentOrder()
+      const state = ethers.utils.formatUnits(await order.state(), 1) * 10
+      if(state < 4) {
+        throw new Error("Waiting for the order to be delivered")
+        return;
+      }
+      const abiCoder = new ethers.utils.AbiCoder()
+      const deliveryData = JSON.parse(abiCoder.decode(["string"], await order.deliveryData())[0])
+      let localKey = localStorage.getItem("easkp")
+  
+      // console.info('deliveryData', deliveryData, localKey)
+      const decryptDeliveryData = async () => {
+        try {
+
+          const key = JSON.parse(JSON.parse(localKey).privateKey)
+          const decryptedKey = await utils.decryptAesKey(deliveryData.encryptedKey, key)
+          const payload = JSON.parse(await utils.decryptPayload(decryptedKey, deliveryData.encryptedPayload, deliveryData.iv))
+          // console.info('dec payload', payload)
+          await Promise.all(payload.map(async (f, i)=>{
+            let decrypted = []
+            if(f.name) {
+              decrypted = new File([await utils.decryptPart(f)], {name:f.name})
+            } else {
+              const parts  = await Promise.all(f.map(async ff=>({...ff, decrypted: await utils.decryptPart(ff)})))
+              parts.sort((a,b)=>a.partId - b.partId)
+              const pmap = utils.spliceBuffers(parts.map(p=>p.decrypted))
+              // console.info('parts dec', pmap)
+              decrypted = new File([pmap], {name: parts[0].name})
+            }
+  
+            const file = decrypted
+            const a = document.createElement("a");
+            a.href = URL.createObjectURL(file);
+            a.setAttribute("download", f.name || f[0].name);
+            document.body.appendChild(a);
+            a.click();
+            document.body.removeChild(a);
+          }))
+          // console.info('decrypted payload', payload)
+        } catch(err) {
+          console.error('decrypt error', err.message)
+          alert(err?.data?.message || err?.message || err)
+        }
+      }
+      if(localKey) {
+        await decryptDeliveryData()
+      }
+      else {
+        localKey = prompt('please attach the decryption key', '')
+        await decryptDeliveryData()
+      }
+      await order.confirmReceived()
+      return {succes:true};
+    } catch(err) {
+      throw err
+      // alert(err?.data?.message || err?.message || err)
+    }
+  }
+  
+  
+  async function checkMetaMask() {
+    if (typeof window.ethereum !== 'undefined') {
+      console.log('MetaMask is installed!');
+      try {
+        // Request account access
+        await window.ethereum.request({ method: 'eth_requestAccounts' });
+        provider = new ethers.providers.Web3Provider(window.ethereum);
+        signer = provider.getSigner();
+        address = await signer.getAddress();
+  
+        // Switch network
+        try {
+          await window.ethereum.request({
+            method: 'wallet_switchEthereumChain',
+            params: [utils.networks[n].chainId], 
+          });
+  
+        } catch (switchError) {
+          // This error code indicates that the chain has not been added to MetaMask.
+          if (switchError.code === 4902) {
+            try {
+              await window.ethereum.request({
+                method: 'wallet_addEthereumChain',
+                params: [utils.networks[n]]
+              });
+            } catch (addError) {
+              console.error('Failed to add the BSC Testnet:', addError);
+            }
+          }
+        }
+        return getWalletInfo()
+        // await updateWalletInfo();
+      } catch (error) {
+        console.error('Failed to connect to MetaMask:', error);
+      }
+    } else {
+      throw 'MetaMask is not installed!'
+    }
+  }
+
+  const connectToExchangeToken = async () =>
+    new ethers.Contract(
+      exchangeTokenAddress(),
+      ExchangeToken__factory.abi,
+      signer,
+    );
+
+  const connectToExchangeService = async (address) =>
+    new ethers.Contract(
+      address,
+      utils.exchangeContractAbi,
+      signer,
+    );
+  
+  const getServicePaymentInfo = async (
+    serviceAddress,
+  ) => {
+    const shop = await connectToExchangeService(serviceAddress);
+    const coin = await shop.wp();
+    const sig = signer
+    const erc20 = new ethers.Contract(coin, ERC20__factory.abi, sig);
+    const decimals = ethers.toBigInt(10) ** (await erc20.decimals());
+    const price = await shop.defaultPrice();
+    const subInterval = ethers.toNumber(await shop.requiresSubs());
+    const value = ethers.toNumber(price / decimals);
+    return {
+      coinName: await erc20.symbol(),
+      coinAddress: coin,
+      subInterval,
+      value,
+    };
+  };
+
+  const checkServiceValid = async (serviceAddress) => {
+    try {
+      const shop = await connectToExchangeService(serviceAddress);
+      const userContractAddress = await shop.userContracts(
+        (
+          signer
+        ).address,
+      );
+      const isValid = await shop.validateSubscription(
+        userContractAddress,
+      );
+      return isValid;
+    } catch (err) {
+      return false;
+    }
+  };
+
+
+  const buyServiceSmart = async (serviceAddress) => {
+    try {
+      const shop = await connectToExchangeService(serviceAddress);
+      const excd = await connectToExchangeToken();
+      const coin = await shop.wp();
+      const sig = signer;
+      const erc20 = new ethers.Contract(coin, utils.erc20abi, sig);
+      const decimals = ethers.toBigInt(10) ** (await erc20.decimals());
+      const price = await shop.defaultPrice();
+      const subInterval = await shop.requiresSubs();
+      const value = ethers.toNumber(price / decimals);
+      const key = await getPremiumKey();
+      const {publicKey} = key.keyPair;
+      const abiCoder = new ethers.AbiCoder();
+      const keyBlob = abiCoder.encode(
+        ['string'],
+        [JSON.stringify({publicKey})],
+      );
+      const prevContractAddress = await shop.userContracts(sig.address);
+      console.info('prevContractAddress', prevContractAddress);
+      const createContract = async () => {
+        await (
+          (await excd.createServiceUserContract(
+            value,
+            shop.wp(),
+            1,
+            shop.provider(),
+            shop.operator(),
+            0,
+            false,
+            0,
+            serviceAddress,
+            serviceAddress,
+          ))
+        ).wait(1);
+
+        const contractAddress = await shop.userContracts(sig.address);
+        const contract = await connectToExchangeService(
+          contractAddress,
+        );
+        return {contract, contractAddress};
+      };
+      if (prevContractAddress === address0) {
+        const {contract, contractAddress} = await createContract();
+        await sendCoin(coin, contractAddress, value);
+        await (await contract.activateService(keyBlob)).wait(1);
+      } else {
+        let contract = await connectToExchangeService(
+          prevContractAddress,
+        );
+        const contractPrice = await contract.defaultPrice();
+        const contractSubInterval = await contract.requiresSubs();
+        if (
+          contractPrice !== price ||
+          contractSubInterval !== subInterval
+        ) {
+          let {contract, contractAddress} = await createContract();
+          await sendCoin(coin, contractAddress, value);
+          await (await contract.activateService(keyBlob)).wait(1);
+          return;
+        }
+        const balance = await contract.balanceOfContract();
+        if (balance >= price) {
+          await (await contract.activateService(keyBlob)).wait(1);
+        } else {
+          await sendCoin(coin, prevContractAddress, value);
+          await (await contract.activateService(keyBlob)).wait(1);
+        }
+      }
+    } catch (err) {
+      console.error('buyservicesmart error', err);
+    }
+  };
+
+  async function getWalletInfo() {
+    if (address) {
+      const balance = await provider.getBalance(address);
+      const etherBalance = ethers.utils.formatEther(balance);
+  
+      const usdtContract = new ethers.Contract(erc20ContractAddress, utils.erc20abi, provider);
+      const usdtBalance = await usdtContract.balanceOf(address);
+      const symbol = await usdtContract.symbol()
+      const usdtBalanceFormatted = ethers.utils.formatUnits(usdtBalance, decimals); // Assuming 18 decimals, adjust if different
+  
+      return {
+        textContent:`Address: ${address.slice(0, 6)}...${address.slice(-4)} | ETH: ${parseFloat(etherBalance).toFixed(4)} | ${symbol}: ${parseFloat(usdtBalanceFormatted).toFixed(2)}`,
+        wrapperBalance: usdtBalance,
+        wrapperContract: usdtContract,
+        etherBalance,
+        symbol
+      }
+    } else {
+      return {textContent: 'Wallet not connected'}
+    }
+  }
+  
+  
+  function initIndex(decryptedData) {
+    console.info('decrypteData', decryptedData)
+    products = decryptedData.shopProducts
+    categories = ["All", ...new Set(products.map(i => i.category))]
+    orderKey = typeof decryptedData.ordersPubKey === "string" ? JSON.parse(decryptedData.ordersPubKey) : decryptedData.ordersPubKey
+  }
+  
+  return {
+    ...utils,
+    checkMetaMask,
+    initIndex,
+    getWalletInfo,
+    receiveOrder,
+    buyItemFromShop,
+    getCurrentOrder,
+    rateSeller,
+    getExchangeContractData,
+    categories,
+    products,
+    buyServiceSmart,
+    checkServiceValid,
+    getServicePaymentInfo,
+    inputPassword
+  };
+}
+window.EXDC_SDK = EXDC_SDK
---
+++ util.js
@@ -0,0 +1,389 @@
+export function EXDC_UTILS() {
+
+  const keyEncAlgo = {
+    name: 'RSA-OAEP',
+  };
+  const payloadKeyEncAlgo = {
+    name: 'AES-GCM',
+    length: 256,
+  };
+  const payloadEncAlgo = {
+    name: 'AES-GCM',
+  };
+  
+  const generateNewKeyPair = async (
+    seed,
+  ) => {
+      const keyPair = (await window.crypto.subtle.generateKey(
+        algo,
+        true,
+        ['encrypt', 'decrypt'],
+      ));
+      const kp = {
+        publicKey: JSON.stringify(
+          await window.crypto.subtle.exportKey(
+            'jwk',
+            keyPair.publicKey,
+          ),
+        ),
+        privateKey: JSON.stringify(
+          await window.crypto.subtle.exportKey(
+            'jwk',
+            keyPair.privateKey,
+          ),
+        ),
+      };
+      return kp;
+  }
+  
+  const getHexFromArrayBuffer = (array) => {
+    const hashArray = Array.from(new Uint8Array(array));
+    const digest = hashArray
+      .map(b => b.toString(16).padStart(2, '0'))
+      .join('');
+    return digest;
+  }
+  
+  const encryptAesKey = async (key, pubKey) => {
+    const k = await window.crypto.subtle.importKey(
+      'jwk',
+      pubKey,
+      algo,
+      true,
+      ['encrypt'],
+    );
+    const encoder = new TextEncoder();
+    const encoded = encoder.encode(key);
+    const encrypted = await window.crypto.subtle.encrypt(
+      keyEncAlgo,
+      k,
+      encoded,
+    );
+    return getHexFromArrayBuffer(encrypted);
+  }
+  
+  const decryptAesKey = async(
+    key,
+    privateKey,
+  ) => {
+    try {
+      const k = await window.crypto.subtle.importKey(
+        'jwk',
+        privateKey,
+        algo,
+        true,
+        ['decrypt'],
+      );
+      const encoded = hexToArrayBuffer(key);
+      const decrypted = await window.crypto.subtle.decrypt(
+        keyEncAlgo,
+        k,
+        encoded,
+      );
+      // console.info('decryptedKey', decrypted);
+      const decoder = new TextDecoder();
+      const decode = JSON.parse(decoder.decode(decrypted, {}));
+      // console.info('textdecoder decode', decode);
+      const aesKey = await window.crypto.subtle.importKey(
+        'jwk',
+        decode,
+        payloadKeyEncAlgo,
+        true,
+        ['encrypt', 'decrypt'],
+      );
+      return aesKey;
+    } catch (err) {
+      console.error('decryptkey error', err);
+      throw err;
+    }
+  }
+  
+  async function decryptPayload(
+    k,
+    payload,
+    ivb,
+  ) {
+    const iv = hexToArrayBuffer(ivb);
+    const encoded = hexToArrayBuffer(payload);
+    // console.info('encoded into buffer', encoded);
+    const decrypted = await window.crypto.subtle.decrypt(
+      {...payloadEncAlgo, iv},
+      k,
+      encoded,
+    );
+    // console.info('decryptedPayload', decrypted);
+    const decoder = new TextDecoder();
+    const decode = decoder.decode(decrypted);
+    return decode;
+  }
+  
+  async function decryptPayloadRaw(
+    k,
+    payload,
+    ivb,
+  ) {
+    const iv = hexToArrayBuffer(ivb);
+    const encoded = payload;
+    // console.info('encoded into buffer', encoded);
+    const decrypted = await window.crypto.subtle.decrypt(
+      {...payloadEncAlgo, iv},
+      k,
+      encoded,
+    );
+    return decrypted;
+    // console.info('decryptedPayload', decrypted);
+    // const decoder = new TextDecoder();
+    // const decode = decoder.decode(decrypted);
+    // return decode;
+  }
+  async function generateAesKey() {
+    const key = await window.crypto.subtle.generateKey(
+      payloadKeyEncAlgo,
+      true,
+      ['encrypt', 'decrypt'],
+    );
+    // console.info('aeskey', key);
+    const kp = JSON.stringify(
+      await window.crypto.subtle.exportKey('jwk', key),
+    );
+    return { kp, key };
+  }
+  
+  async function encryptPayload(k, payload) {
+    try {
+      const encoder = new TextEncoder();
+      const encoded = encoder.encode(payload);
+      const iv = window.crypto.getRandomValues(new Uint8Array(12));
+      const encrypted = await window.crypto.subtle.encrypt(
+        { ...payloadEncAlgo, iv },
+        k,
+        encoded,
+      );
+      // console.info('encrypted', encrypted);
+      return {
+        encryptedPayload: await getHexFromArrayBuffer(encrypted),
+        iv: await getHexFromArrayBuffer(iv.buffer),
+      };
+    } catch (err) {
+      console.error('encrypt payload err', err);
+      throw err;
+    }
+  }
+  
+  const algo = {
+    name: 'RSA-OAEP',
+    modulusLength: 4096,
+    publicExponent: new Uint8Array([1, 0, 1]),
+    hash: 'SHA-256',
+  };
+  
+
+  const getKeyMaterial = (password) => {
+    const enc = new TextEncoder();
+    return window.crypto.subtle.importKey(
+      "raw",
+      enc.encode(password),
+      "PBKDF2",
+      false,
+      ["deriveBits", "deriveKey"],
+    );
+  }
+
+  const hexToArrayBuffer = (hex) => {
+    const buff = new Uint8Array(
+      (hex.match(/[\da-f]{2}/gi)).map(function (h) {
+        return parseInt(h, 16);
+      }),
+    );
+    return buff;
+  }
+
+  const decrypt = async (password, json) => {
+    const dec = new TextDecoder()
+    const data = hexToArrayBuffer(json.encrypted)
+    const iv = hexToArrayBuffer(json.iv)
+    const keyMaterial = await getKeyMaterial(password);
+    const salt = hexToArrayBuffer(json.salt)
+    const key = await window.crypto.subtle.deriveKey(
+      {
+        name: "PBKDF2",
+        salt,
+        iterations: 100000,
+        hash: "SHA-256",
+      },
+      keyMaterial,
+      { name: "AES-GCM", length: 256 },
+      true,
+      ["encrypt", "decrypt"],
+    );
+    // console.info("before decrypt", key, data, iv)
+    const decrypted = await window.crypto.subtle.decrypt({ name: "AES-GCM", iv }, key, data);
+    return JSON.parse(dec.decode(decrypted))
+  }
+
+
+  const hex = (arrayBuffer) => {
+      const buff = new Uint8Array(arrayBuffer);
+      const hexOctets = []; // new Array(buff.length) is even faster (preallocates necessary array size), then use hexOctets[i] instead of .push()
+
+      for (let i = 0; i < buff.length; ++i)
+          hexOctets.push(byteToHex[buff[i]]);
+
+      return hexOctets.join("");
+  }
+
+  const decryptPart = async (f) => {
+    const link = "https://ipfs.io/ipfs/"+f.fileLink.IpfsHash
+    const efile = await (await fetch(link)).arrayBuffer()
+    // console.info(efile)
+    const decrypted = await utils.decryptPayloadRaw(decryptedKey, efile, f.iv)
+    // console.info('decrypted', decrypted)
+    return decrypted;
+  }
+
+  const spliceBuffers = (buffers) => {
+
+    const len = buffers.map((buffer) => buffer.byteLength).reduce((prevLength, curr) => {return prevLength + curr}, 0);
+
+    const tmp = new Uint8Array(len);
+
+    let bufferOffset = 0;
+
+    for(var i=0; i < buffers.length; i++) {
+      tmp.set(new Uint8Array(buffers[i]), bufferOffset);
+      bufferOffset += buffers[i].byteLength;
+    }
+
+    return tmp;
+  }
+
+  const erc20abi = [
+    // Read-Only Functions
+    'function balanceOf(address owner) view returns (uint256)',
+    'function decimals() view returns (uint8)',
+    'function symbol() view returns (string)',
+
+    // Authenticated Functions
+    'function transfer(address to, uint amount) returns (bool)',
+
+    // Events
+    'event Transfer(address indexed from, address indexed to, uint amount)',
+  ];
+
+  const exchangeContractAbi = [
+    'function userData() view returns (bytes)',
+    'function wp() view returns (address)',
+    'function decimals() view returns (uint8)',
+    'function buyerOrders(address address) view returns (address)',
+    'function createBuyItemsContract(uint contractValue, uint contractDivider, address paymentContract) external returns (address)'
+  ]
+
+  const buyContractAbi = [
+    'function confirmPurchase(bytes data) external',
+    'function confirmReceived() external',
+    'function rateSeller(uint buyerRating) external',
+    'function balanceOfContract() view returns (uint)',
+    'function price() view returns (uint)',
+    'function state() view returns (uint)',
+    'function deliveryData() view returns (bytes)'
+  ]
+
+
+  const exchangeTokenAddress = () =>
+    networkToContract[window.ethereum.networkVersion];
+
+  const networkToContract = {
+    80002: '0x863D66d6692FE5D2e422DF771750cb14295a4D02',
+    97: '0x33011291740eD60C5F083F57c38439afB30346Bc',
+    137: '0xD75dc076cDB0E692214371924B6640c545F688C6',
+    56: '0xAbC58cfbB3c71d92665b96125e05c20dC4aCe51A'
+  };
+
+  const networks = {
+    1: {
+      chainId: '0x1',
+      chainName: 'Ethereum Mainnet',
+      nativeCurrency: {
+        name: 'Ethereum',
+        symbol: 'ETH',
+        decimals: 18,
+      },
+      rpcUrls: ['https://eth.llamarpc.com'],
+      blockExplorerUrls: ['https://etherscan.io'],
+    },
+    137: {
+      chainId: '0x89',
+      chainName: 'Polygon Mainnet',
+      nativeCurrency: {
+        name: 'MATIC',
+        symbol: 'MATIC',
+        decimals: 18,
+      },
+      rpcUrls: ['https://polygon-mainnet.infura.io'],
+      blockExplorerUrls: ['https://polygonscan.com/'],
+    },
+    324: {
+      chainId: '0x144',
+      chainName: 'zkSync Mainnet',
+      nativeCurrency: {
+        name: 'Ether',
+        symbol: 'ETH',
+        decimals: 18,
+      },
+      rpcUrls: ['https://mainnet.era.zksync.io'],
+      blockExplorerUrls: ['https://era.zksync.network/'],
+    },
+    56: {
+      chainId: '0x38',
+      chainName: 'BNB Smart Chain',
+      nativeCurrency: {
+        name: 'BNB',
+        symbol: 'BNB',
+        decimals: 18,
+      },
+      rpcUrls: ['https://bsc-dataseed.binance.org/'],
+      blockExplorerUrls: ['https://bscscan.com'],
+    },
+    97: {
+      chainId: '0x61',
+      chainName: 'BNB Smart Chain Testnet',
+      nativeCurrency: {
+        name: 'TBNB',
+        symbol: 'TBNB',
+        decimals: 18,
+      },
+      rpcUrls: ['https://data-seed-prebsc-1-s1.binance.org:8545/'],
+      blockExplorerUrls: ['https://testnet.bscscan.com'],
+    },
+    80002: {
+      chainId: '0x13882',
+      chainName: 'Polygon Amoy Testnet',
+      nativeCurrency: {
+        name: 'MATIC',
+        symbol: 'MATIC',
+        decimals: 18,
+      },
+      rpcUrls: ['https://rpc-amoy.polygon.technology'],
+      blockExplorerUrls: ['https://www.oklink.com/amoy'],
+    },
+  };
+  return {
+    networks,
+    networkToContract,
+    hex,
+    hexToArrayBuffer,
+    erc20abi,
+    decrypt,
+    exchangeContractAbi,
+    buyContractAbi,
+    spliceBuffers,
+    encryptAesKey,
+    encryptPayload,
+    decryptPayload,
+    decryptPayloadRaw,
+    generateNewKeyPair,
+    decryptPart,
+    generateAesKey,
+    exchangeTokenAddress
+  }
+}
